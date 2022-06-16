5bc0fc62b3cc8094ff7e16b7d33d47e2dc726164
Sayt <sayt@thethunderdome.net>
2022-06-15 23:26:25 -0500

    surf_autism zone fix
    Quick fix to correctly zone bonus 1.

--- Zones/Sorted - By Tier/Tier 1/surf_autism_2_maptier.sql
+++ Zones/Sorted - By Tier/Tier 1/surf_autism_2_maptier.sql
@@ -1,4 +1,4 @@
 DELETE FROM `ck_maptier` WHERE mapname = 'surf_autism_2';
 
 INSERT INTO `ck_maptier` (`mapname`,`mapper`,`tier`,`maxvelocity`,`announcerecord`,`gravityfix`,`ranked`) VALUES 
-('surf_autism_2','Titty Sprinkles',1,3500,0,1,1);
+('surf_autism_2','Titty Sprinkles',1,3500,1,1,1);
